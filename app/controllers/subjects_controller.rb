5e158b48980bf01d53a4ccbafcadb2d0f072b353
Taisei <taisei25864@gmail.com>
2022-03-22 01:09:01 +0900

    creat thread perfectly

--- app/controllers/subjects_controller.rb
+++ app/controllers/subjects_controller.rb
@@ -1,8 +1,10 @@
 class SubjectsController < ApplicationController
     def index
+        @subjects = Subject.all
     end
 
     def show
+        @subject = Subject.find(params[:id])
     end
 
     def new
@@ -10,14 +12,35 @@ class SubjectsController < ApplicationController
     end
 
     def create
+        @subject = Subject.new(subject_params)
+        if @subject.save
+            redirect_to @subject
+        else
+            render 'new', status: :unprocessable_entity
+        end
     end
 
     def edit
+        @subject = Subject.find(params[:id])
     end
 
     def update
+        @subject = Subject.find(params[:id])
+        if @subject.update(subject_params)
+            redirect_to @subject
+        else
+            render 'edit', status: :unprocessable_entity
+        end
     end
 
     def destroy
+        @subject = Subject.find(params[:id])
+        @subject.destroy
+        redirect_to subject_path
+    end
+
+    private
+    def subject_params
+        params.require(:subject).permit(:subject_name, :teacher_name)
     end
 end
--- app/models/subject.rb
+++ app/models/subject.rb
@@ -1,2 +1,4 @@
 class Subject < ApplicationRecord
+    validates :subject_name, presence: true
+    validates :teacher_name, presence: true
 end
